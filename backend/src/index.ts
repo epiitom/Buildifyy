9c3154660b3ced41871d25728b9e40a3a1e49293
Prathmesh Kale <PRATHMESHKALE0018@GMAIL.COM>
2025-06-05 20:10:06 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,73 @@
+require ("dotenv").config();
+import OpenAI from "openai";
+import {BASE_PROMPT,getSystemPrompt} from "./prompts"
+import {basePrompt as nodeBasePrompt} from "./defaults/node";
+import {basePrompt as reactBasePrompt} from "./defaults/react";
+import express, {Request,Response} from "express"
+const app = express();
+
+app.use(express.json())
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, 
+});
+
+app.get("/template",async (req:Request, res:Response) => {
+    const prompt = req.body.prompt;
+   
+    const response = await client.chat.completions.create({
+           model: "gpt-4",
+        messages :[
+          {
+          role: 'user' , content: prompt
+        },
+         {
+           role: 'system',
+             content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra." 
+            }       
+],
+  })
+
+   const answer = response.choices[0].message?.content?.trim();
+     if(answer == "react"){
+        res.json({
+            prompts: [BASE_PROMPT,reactBasePrompt]
+        })
+        return;
+     }
+     if(answer == "node"){
+        res.json({
+             prompts:[nodeBasePrompt]
+        })
+        return;
+     }
+         res.status(403).json({message: "You cant access this"})
+     return;
+
+})
+app.listen(3000)
+
+    
+
+// async function main(){
+// const response = await client.chat.completions.create({
+//     model: "gpt-4",
+//      messages: [
+//         {
+//          role:"system",
+//          content:  getSystemPrompt(),   
+//      },
+//      {
+//         role: "user",
+//        content: "create a todo app"
+//      }
+//     ],
+//     stream:true,
+   
+// });
+
+// for await (const chunk of response) {
+//  console.log((chunk.choices[0]?.delta?.content || ""));
+// }
+// }
+// main()   
